0fda876fc20e2ec11865129487fd78bcbdf6e2dc
JordanPostak <jordan_postak@hotmail.com>
2023-07-07 23:06:16 -0600

    working on Bountiful Website

--- Bountiful/scripts/hamburger.js
+++ Bountiful/scripts/hamburger.js
@@ -1,5 +1,10 @@
 const hamburger = document.querySelector('.hamburger');
+const navContainer = document.querySelector('.nav-closed');
+const header = document.querySelector('header');
+const mainSection = document.querySelector('main');
 
 hamburger.addEventListener('click', function() {
   this.classList.toggle('active');
+  navContainer.classList.toggle('nav-open');
+  mainSection.classList.toggle('main-up');
 });

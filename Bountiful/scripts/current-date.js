ebbe487003e8b174655f43665c5ef5ef19e16f25
JordanPostak <jordan_postak@hotmail.com>
2023-07-08 23:56:28 -0600

    home page content

--- Bountiful/scripts/current-date.js
+++ Bountiful/scripts/current-date.js
@@ -6,19 +6,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
     dateElement.textContent = currentDate;
 
-     // Creates a new Date object and retrieves the current year from it
      const currentYear = new Date().getFullYear();
 
-     // Retrieves the HTML element with the ID "year" and stores it in the "yearSpan" variable
      const yearSpan = document.querySelector("#year");
  
-     // Sets the text content of the "yearSpan" element to the current year retrieved earlier
      yearSpan.textContent = currentYear;
  
-     // Retrieves the "lastModified" property of the document and stores it in the "lastModified" variable
      const lastModified = document.lastModified;
  
-     // Retrieves the HTML element with the ID "lastModified" and sets its innerHTML to a string that says "Last updated: " followed by the date and time when the document was last modified
      document.querySelector("#lastModified").innerHTML = "Last updated: " + lastModified;
-     
 })
--- Bountiful/scripts/hamburger.js
+++ Bountiful/scripts/hamburger.js
@@ -2,9 +2,11 @@ const hamburger = document.querySelector('.hamburger');
 const navContainer = document.querySelector('.nav-closed');
 const header = document.querySelector('header');
 const mainSection = document.querySelector('main');
+const footerSection = document.querySelector('footer');
 
 hamburger.addEventListener('click', function() {
   this.classList.toggle('active');
   navContainer.classList.toggle('nav-open');
   mainSection.classList.toggle('main-up');
+  footerSection.classList.toggle('footer-up');
 });
